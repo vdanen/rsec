0652653697740d4ea719712e4aba89b11ae3e2de
Vincent Danen <vdanen@linsec.ca>
2004-04-24 02:49:15 +0000

    make it a bit more robust

--- cron-sh/urpmicheck.sh
+++ cron-sh/urpmicheck.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
-
+#
 # Update urpmi medium and report on any newly found packages
+#
+# Written by Vincent Danen <vdanen@opensls.org>
+
+if [[ -f /etc/security/rsec.conf ]]; then
+    . /etc/security/rsec.conf
+else
+    echo "/etc/security/rsec.conf doesn't exist."
+    exit 1
+fi
+
+TMP=`mktemp /tmp/secure.XXXXXX`
 
 urpmi.update -a >/dev/null 2>&1
 
@@ -10,19 +21,27 @@ HOST=`hostname`
 DATE=`date`
 HEAD=0
 
-HEADER="OpenSLS package updates monitor\n\nCheck performed on host $HOST on $DATE\n\nThe following updates are available for your system.  To install these\nupdates, please execute 'urpmi --auto-select' on your system.\n\n"
-
 for i in $MEDIA
 do
     LIST=`urpmq --auto-select --media $i`
     if [ "$LIST" != "" ]; then
-	if [ "$HEAD" == "0" ]; then
-	    echo -e $HEADER
-	    HEAD=1
-	fi
-	echo "The following updates in media '$i' were found:"
-	echo -n " "
-	echo $LIST
-	echo ""
+        if [ "$HEAD" -eq 0 ]; then
+            printf "\nOpenSLS package updates monitor\n\n" >> $TMP
+            printf "Check performed on $HOST on $DATE\n\n" >> $TMP
+            printf "The following updates are available for your system.  To install these\n" >> $TMP
+            printf "updates, please execute 'urpmi --auto-select' on your system.\n\n" >> $TMP
+            HEAD=1
+        fi
+	printf "The following updates in media '$i' were found:\n\n" >> $TMP
+	printf "$LIST\n\n" >> $TMP
     fi
 done
+
+# only mail the report if there is something worth mailing
+if [[ -s $TMP ]]; then
+    cat $TMP | /bin/mail -s "[rsec] *** Package Updates Check on $HOST, $DATE ***" "${MAIL_USER}"
+fi
+
+if [[ -f $TMP ]]; then
+	rm -f $TMP
+fi
