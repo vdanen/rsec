0d1697df737a13a60de703359e9db8b227e40068
Vincent Danen <vdanen@linsec.ca>
2007-02-04 02:19:42 +0000

    - add a cronjob to clean apt's cache

---
+++ cron-sh/apt_cleancache.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# clean the apt cache every night; if this isn't done regularly it can
+# start to eat away at /var
+#
+# of course, if you don't want to automatically remove the cache, delete
+# /etc/cron.daily/apt_cleancache.sh
+
+apt-get clean >/dev/null 2>&1
+
